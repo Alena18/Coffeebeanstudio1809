225592df6a609b48727a26c8aa1b13c392190189
Alena18 <>
2023-05-24 09:16:15 +0000

    Add profile url

--- coffeebeanstudio1809/urls.py
+++ coffeebeanstudio1809/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('design.urls')),
+    path('', include('design.urls'), name='design_urls'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
