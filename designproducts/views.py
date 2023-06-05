6f4fd75d59bd0bc63288153dc65abc62d8e7b93d
Alena18 <>
2023-06-05 20:51:46 +0000

    Create view designproducts

--- designproducts/views.py
+++ designproducts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+
+    """ A view to show all products, including sort and search queries """
+
+    designproducts = Product.objects.all()
+
+    context = {
+        'designproducts': designproducts,
+    }
+
+    return render(request, 'designproducts/designproducts.html', context)
+
