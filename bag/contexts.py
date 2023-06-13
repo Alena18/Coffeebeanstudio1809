e5d35340c79e440a76cdf6f997f1c5fb940637ea
Alena18 <>
2023-06-13 20:06:45 +0000

    Update styles for sort button

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,36 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from designproducts.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    designproduct_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            designproduct = get_object_or_404(Product, pk=item_id)
+            total += item_data * designproduct.price
+            designproduct_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'designproduct': designproduct,
+            })
+        else:
+            designproduct = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * designproduct.price
+                designproduct_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'designproduct': designproduct,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -19,7 +44,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'designproduct_count': designproduct_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
