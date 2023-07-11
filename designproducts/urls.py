33cbd9e2f179f3f07071eda0961f8421b7b6afbb
Alena18 <>
2023-07-11 20:32:56 +0000

    Update delete and edit functionality

--- designproducts/urls.py
+++ designproducts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.all_products, name='designproducts'),
     path('<int:designproduct_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_designproduct, name='add_designproduct'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('edit/<int:designproduct_id>/', views.edit_designproduct, name='edit_designproduct'),
+    path('delete/<int:designproduct_id>/', views.delete_designproduct, name='delete_designproduct'),
     ]
--- designproducts/views.py
+++ designproducts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -41,7 +42,7 @@ def all_products(request):
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+                return redirect(reverse('designproducts'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             designproducts = designproducts.filter(queries)
@@ -70,8 +71,13 @@ def product_detail(request, designproduct_id):
     return render(request, 'designproducts/product_detail.html', context)
 
 
+@login_required
 def add_designproduct(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))    
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -90,25 +96,44 @@ def add_designproduct(request):
 
     return render(request, template, context)
 
+
+@login_required
 def edit_designproduct(request, designproduct_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     designproduct = get_object_or_404(Product, pk=designproduct_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=designproduct)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('designproduct_detail', args=[designproduct.id]))
+            return redirect(reverse('product_detail', args=[designproduct.id]))
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=designproduct)
         messages.info(request, f'You are editing {designproduct.name}')
 
-    template = 'products/edit_designproduct.html'
+    template = 'designproducts/edit_designproduct.html'
     context = {
         'form': form,
         'designproduct': designproduct,
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_designproduct(request, designproduct_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=designproduct_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('designproducts'))
