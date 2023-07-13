b5fdf3d8871376edcd45227ed7822cc329d4876b
Alena18 <>
2023-07-13 12:56:37 +0000

    Add setting.py file to depluy

--- coffeebeanstudio1809/settings.py
+++ coffeebeanstudio1809/settings.py
@@ -170,7 +170,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
@@ -178,6 +177,12 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'coffeebeanstudio1809'
     AWS_S3_REGION_NAME = 'eu-west-1'
